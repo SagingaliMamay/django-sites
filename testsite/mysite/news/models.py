8f7b002a790c5f075ca7a6f55905951a2c1daa33
Sagy17 <sagingali.mamayev@gmail.com>
2021-06-30 10:31:59 +0200

    fields done

--- testsite/mysite/news/models.py
+++ testsite/mysite/news/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+
+
 # Create your models here.
+# creation fields of my DB
 
 class News(models.Model):
-    title
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
+
+
